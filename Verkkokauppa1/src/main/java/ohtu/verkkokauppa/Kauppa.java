54125d10cc5fa730433f10cafc8c23764fa56329
Tuomas Koukkari <tuomas.koukkari@helsinki.fi>
2020-11-09 23:49:03 +0200

    v2 t9-10 c1: verkkokauppa

---
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,42 @@
+package ohtu.verkkokauppa;
+
+public class Kauppa {
+
+    private VarastoRajapinta varasto;
+    private PankkiRajapinta pankki;
+    private Ostoskori ostoskori;
+    private ViiteRajapinta viitegeneraattori;
+    private String kaupanTili;
+
+    public Kauppa(VarastoRajapinta v, PankkiRajapinta p, ViiteRajapinta g) {
+        varasto = v;
+        pankki = p;
+        viitegeneraattori = g;
+        kaupanTili = "33333-44455";
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id); 
+        varasto.palautaVarastoon(t);
+    }
+
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id)>0) {
+            Tuote t = varasto.haeTuote(id);             
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+        
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
