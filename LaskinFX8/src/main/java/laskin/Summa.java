5f78ab6fe5a3d67d54cc9b2130577fa0213bee4d
Tuomas Koukkari <tuomas.koukkari@helsinki.fi>
2020-11-30 21:17:21 +0200

    v5 t4

---
+++ LaskinFX8/src/main/java/laskin/Summa.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author tkoukkar
+ */
+public class Summa extends Komento {
+
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int arvo = 0;
+ 
+        try {
+            arvo = Integer.parseInt(syotekentta.getText());
+        } catch (Exception e) {
+        }
+        
+        sovellus.plus(arvo);
+        
+        int laskunTulos = sovellus.tulos();
+        
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+        
+        if ( laskunTulos==0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+
+    @Override
+    public void peru() {
+        System.out.println("undo pressed");
+    }
+    
+}
